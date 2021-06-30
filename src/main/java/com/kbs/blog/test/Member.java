a36be475bde90971704f99d690ed12311341921e
kbs <chorocsky@gmail.com>
2021-06-30 22:07:55 +0900

    생성자 추가

--- src/main/java/com/kbs/blog/test/Member.java
+++ src/main/java/com/kbs/blog/test/Member.java
@@ -1,10 +1,13 @@
 package com.kbs.blog.test;
 
 public class Member {
+  
   private int id;
   private String username;
   private String password;
   private String email;
+  
+  public Member() {}
   
   public Member(int id, String username, String password, String email) {
     this.id = id;
