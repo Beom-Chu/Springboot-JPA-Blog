1ae742532a46eccdf0f13f8d094e2a5c55644778
kbs <chorocsky@gmail.com>
2021-07-05 22:37:15 +0900

    RoleType Enum으로 설정

--- src/main/java/com/kbs/blog/model/User.java
+++ src/main/java/com/kbs/blog/model/User.java
@@ -3,31 +3,49 @@ package com.kbs.blog.model;
 import java.sql.Timestamp;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import org.hibernate.annotations.ColumnDefault;
 import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.DynamicInsert;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
 
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@ToString
 @Entity
+//@DynamicInsert //insert시 Null인 필드 제외
 public class User {
 
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY) // 프로젝트에서 연결된 DB의 넘버링 전략을 따름.
   private int id;
-  
+
   @Column(nullable = false, length = 30)
   private String username;
-  
+
   @Column(nullable = false, length = 100)
   private String password;
-  
+
   @Column(nullable = false, length = 50)
   private String email;
-  
-  @ColumnDefault("'user'")
-  private String role;
-  
+
+//  @ColumnDefault("'user'")
+//  private String role;
+  @Enumerated(EnumType.STRING)
+  private RoleType role;
+
   @CreationTimestamp
   private Timestamp createDate;
 }
