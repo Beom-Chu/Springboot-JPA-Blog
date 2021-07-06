8dd147a3b9f103bce6769eeed00cfebf665cecfb
kbs <chorocsky@gmail.com>
2021-07-06 22:23:18 +0900

    super() 제거

--- src/main/java/com/kbs/blog/test/Member.java
+++ src/main/java/com/kbs/blog/test/Member.java
@@ -15,7 +15,6 @@ public class Member {
   
   @Builder
   public Member(int id, String username, String password, String email) {
-    super();
     this.id = id;
     this.username = username;
     this.password = password;
