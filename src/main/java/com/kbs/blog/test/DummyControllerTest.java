9b0c4f28b73dc8fa95cb6ad66e0b07172575f8a9
kbs <chorocsky@gmail.com>
2021-07-13 14:32:31 +0900

    삭제 추가

--- src/main/java/com/kbs/blog/test/DummyControllerTest.java
+++ src/main/java/com/kbs/blog/test/DummyControllerTest.java
@@ -3,10 +3,12 @@ package com.kbs.blog.test;
 import java.util.List;
 import javax.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -89,5 +91,15 @@ public class DummyControllerTest {
     user.setPassword(requestUser.getPassword());
 
     return "수정이 완료 되었습니다.";
+  }
+  
+  @DeleteMapping("/user/delete/{id}")
+  public String delete(@PathVariable int id) {
+    try {
+      userRepository.deleteById(id);
+    }catch (EmptyResultDataAccessException e) {
+      return "삭제 실패. 삭제 대상이 존재하지 않습니다.";
+    }
+    return "삭제 되었습니다.";
   }
 }
