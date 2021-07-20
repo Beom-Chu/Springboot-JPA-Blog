428db3a4391b56d3592a1a87284ff04a77b457a7
kbs <chorocsky@gmail.com>
2021-07-20 18:58:44 +0900

    UserService DI

--- src/main/java/com/kbs/blog/controller/api/UserApiController.java
+++ src/main/java/com/kbs/blog/controller/api/UserApiController.java
@@ -1,20 +1,33 @@
 package com.kbs.blog.controller.api;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import com.kbs.blog.dto.ResponseDto;
+import com.kbs.blog.model.RoleType;
 import com.kbs.blog.model.User;
+import com.kbs.blog.service.UserService;
 
 @RestController
 public class UserApiController {
 
+  private final UserService userService;
+
+  @Autowired
+  public UserApiController(UserService userService) {
+    this.userService = userService;
+  }
+
   @PostMapping("/api/user")
   public ResponseDto<Integer> save(@RequestBody User user) {
-    
+
     System.out.println("api/user");
-    
-    return new ResponseDto<Integer>(HttpStatus.OK, 1);
+
+    user.setRole(RoleType.USER);
+    int result = userService.회원가입(user);
+
+    return new ResponseDto<Integer>(HttpStatus.OK, result);
   }
 }
