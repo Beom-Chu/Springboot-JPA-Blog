e91dc2fcb4c541c810f4b94b858dc09e86bdc572
kbs <chorocsky@gmail.com>
2021-08-09 18:50:52 +0900

    스웨거 페이징 관련 설정

--- src/main/java/com/kbs/blog/config/SwaggerConfig.java
+++ src/main/java/com/kbs/blog/config/SwaggerConfig.java
@@ -1,10 +1,16 @@
 package com.kbs.blog.config;
 
+import java.util.List;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.domain.Pageable;
+import com.fasterxml.classmate.TypeResolver;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.schema.AlternateTypeRules;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
@@ -19,9 +25,16 @@ public class SwaggerConfig {
   private static final String API_DESCRIPTION = "BeomChu Blog API 입니다.";
   private static final String API_GROUP_NAME = "그룹1";
   
+  private final TypeResolver typeResolver;
+  
+  public SwaggerConfig(TypeResolver typeResolver) {
+    this.typeResolver = typeResolver;
+  }
+  
   @Bean
   public Docket api() {
     return new Docket(DocumentationType.SWAGGER_2)
+        .alternateTypeRules(AlternateTypeRules.newRule(typeResolver.resolve(Pageable.class), typeResolver.resolve(Page.class)))
               .apiInfo(apiInfo())
               .groupName(API_GROUP_NAME)
               .select()
@@ -36,5 +49,41 @@ public class SwaggerConfig {
               .version(API_VERSION)
               .description(API_DESCRIPTION)
               .build();
+  }
+  
+  @ApiModel
+  static class Page {
+      @ApiModelProperty(value = "페이지 번호(0..N)", example = "0")
+      private Integer page;
+
+      @ApiModelProperty(value = "페이지 크기", allowableValues="range[0, 100]", example = "0")
+      private Integer size;
+
+      @ApiModelProperty(value = "정렬(사용법: 컬럼명,ASC|DESC)")
+      private List<String> sort;
+
+      public Integer getPage() {
+        return page;
+      }
+
+      public void setPage(Integer page) {
+        this.page = page;
+      }
+
+      public Integer getSize() {
+        return size;
+      }
+
+      public void setSize(Integer size) {
+        this.size = size;
+      }
+
+      public List<String> getSort() {
+        return sort;
+      }
+
+      public void setSort(List<String> sort) {
+        this.sort = sort;
+      }
   }
 }
